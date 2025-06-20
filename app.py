6b66d8d1f394c68608fdcad3889a74e2bd5fa7d9
Samantha <atituanap1@est.ups.edu.ec>
2025-06-20 17:40:52 -0500

    se controla el  flujo principal para capturar errores no controlados

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ libro = LibroDiario()
 #libro.agregar_transaccion('18/06/2025', 'Compra de laptop', 780, 'egreso')
 #libro.agregar_transaccion('18/06/2025', 'Venta de sensor TK-110', 780, 'Ingreso')
 #libro.agregar_transaccion('18/06/2025', 'Compra de insumos de oficina', -85.60, 'egreso')
+
 try:
     libro.cargar_transacciones_desde_archivo("datos.csv")
 ##print(libro.calcular_resumen())
